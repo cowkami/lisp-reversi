ca905e72b4c917caead731eceea6f27fe4dbc548
lee <lee.unseo17@gmail.com>
2019-08-20 00:04:35 +0900

    developing display-board

--- main.lisp
+++ main.lisp
@@ -3,10 +3,26 @@
 (defvar black-board #b0000000000000000000000000000100000010000000000000000000000000000)
 (defvar white-board #b0000000000000000000000000001000000001000000000000000000000000000)
 
-(defun convert-bit2-list)
+;;; return whether Ith bit-board is 1 or 0.
+(defun ith-flag (board i)
+  (if (> (logand 1 (ash board (- i))) 0)
+      t
+      nil))
 
-(defun print-board (board1 board2)
-  (format nil "~D" (logior board1 board2)))
+(defun bits2color (black-flag white-flag)
+  (cond ((assert (not (and black-flag white-flag))
+                 nil 
+                 "Two stones are on one place."))
+        ((not (or black-flag white-flag)) ".")
+        (black-flag "o")
+        (white-flag"x")))
+
+(defun bit-board2str (b-board w-board &optional (i 0))
+  (let ((b-flag (ith-flag b-board i))
+        (w-flag (ith-flag w-board i)))
+       (concatenate 'string
+                    (bits2color b-flag w-flag)
+                    "aoeu")))
 
 (defun main()
   (let ((black-board ))))
