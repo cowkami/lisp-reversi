5a7bbca154c48820ebcf4ffd0b0127220ec67c2d
lee <lee.unseo17@gmail.com>
2019-08-20 01:20:22 +0900

    add functions to display game board.

--- main.lisp
+++ main.lisp
@@ -18,11 +18,18 @@
         (white-flag"x")))
 
 (defun bit-board2str (b-board w-board &optional (i 0))
-  (let ((b-flag (ith-flag b-board i))
-        (w-flag (ith-flag w-board i)))
-       (concatenate 'string
-                    (bits2color b-flag w-flag)
-                    "aoeu")))
+  (if (> i 63)
+      nil
+      (let ((b-flag (ith-flag b-board i))
+            (w-flag (ith-flag w-board i)))
+           (concatenate 'string
+               (when (= i 0)
+                     "*12345678*~%")
+               (when (= (mod i 8) 0)
+                     (string (code-char (+ (char-code #\a) (floor i 8)))))
+               (bits2color b-flag w-flag)
+               (when (= (mod i 8) 7) "~%")
+               (bit-board2str b-board w-board (+ i 1))))))
 
 (defun main()
   (let ((black-board ))))
